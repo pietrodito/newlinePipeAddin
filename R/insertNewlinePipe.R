983f938a78450d1bd876101769ff8f7aab267324
Pierre Balaye <pierre@balaye.fr>
2023-07-04 13:55:08 +0000

    Add insert parentheses over three lines

--- R/insertNewlinePipe.R
+++ R/insertNewlinePipe.R
@@ -1,3 +1,18 @@
 insertNewlinePipe <- function() {
   rstudioapi::insertText("\n  %>% ")
 }
+
+insertParenthesisedPipeWorkflow <- function() {
+  go_back_to_end_of_previous_line <- function() {
+    context <- rstudioapi::getActiveDocumentContext()
+    current_row <- context$selection[[1]]$range$start[[1]]
+    rstudioapi::setCursorPosition(rstudioapi::document_position(
+      row = current_row - 1,
+      column = Inf
+    ))
+  }
+
+  rstudioapi::insertText("(\n  \n)")
+  go_back_to_end_of_previous_line()
+}
+
